fec89bd3749859b6c5aa5a32ae401ff9f2da2f6e
Chad Everett <github@jayseae.cxliv.org>
2009-12-05 12:46:33 -0500

    Release - 3.4.6os, Updated license to artistic

--- trunk/plugins/Notifier/lib/Notifier/Data.pm
+++ trunk/plugins/Notifier/lib/Notifier/Data.pm
@@ -1,5 +1,17 @@
 # ===========================================================================
-# Copyright Everitz Consulting.  Not for redistribution.
+# A Movable Type plugin with subscription options for your installation
+# Copyright 2003, 2004, 2005, 2006 Everitz Consulting <everitz.com>.
+#
+# This program is free software:  You may redistribute it and/or modify it
+# it under the terms of the Artistic License version 2 as published by the
+# Open Source Initiative.
+#
+# This program is distributed in the hope that it will be useful but does
+# NOT INCLUDE ANY WARRANTY; Without even the implied warranty of FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+# You should have received a copy of the Artistic License with this program.
+# If not, see <http://www.opensource.org/licenses/artistic-license-2.0.php>.
 # ===========================================================================
 package Notifier::Data;
 
